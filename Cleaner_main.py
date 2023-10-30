91d33031f376dd1674a969de4c10f334867c6b91
Bruhout <147948392+Bruhout@users.noreply.github.com>
2023-10-30 23:13:57 +0530

    Create Cleaner_main.py

---
+++ Cleaner_main.py
@@ -0,0 +1,43 @@
+import openpyxl as xl
+import pandas as pd
+import numpy as np
+
+
+#setup
+path=str(input("Path: "))
+sheetname=str(input("Worksheet: "))
+column=str(input("Header: "))
+lenght=int(input("Lenght: "))
+
+def __main__(path,sheetname,column,lenght):
+    #Creating a new list with only the float values
+        #pandas to read dataframe
+    data=pd.read_excel(path)
+    new=[]
+    #Iterating through the column
+    for i in data[column].values:
+        strtofloat=""
+        #Already floats dont get edited
+        if isinstance(i,int) or isinstance(i,float) or i=='':
+            new.append(float(i))
+        #Strings get extra characters stripped and converted to floats
+        elif isinstance(i,str):
+            for p in i:
+                if p in "1234567890.":
+                    strtofloat=strtofloat+p
+            new.append(float(strtofloat))
+    new1=np.array(new)
+
+    file=xl.load_workbook(path)
+    sheet=file.get_sheet_by_name(sheetname)
+
+    #Iterating through the columns and replacing values
+    def change_cell_column(lenght):
+        for i in range (2,lenght+2):
+            sheet[f'H{i}']=new[i-2]
+
+    change_cell_column(lenght)
+
+    file.save(path)
+
+__main__(path,sheetname,column,lenght)
